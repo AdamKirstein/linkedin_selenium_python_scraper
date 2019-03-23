b4cdd7ed44fef1c1a3fb15df8ff221ba68e91a2a
Adam Kirstein <ank2157@columbia.edu>
2019-03-23 17:57:30 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,3 +1,21 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+from random import randint
+import requests
+import pandas as pd
+import re
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 time_delay = randint(3,6)
 
 #prepare linkedin login creads
